0eecad1cb99f7569f8e76cdf73c2b97156349313
ghostfart <appstore@fvnction.net>
2022-06-13 12:25:53 +0200

    add html files' loaing

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,13 +1,19 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
+use std::fs;
 pub struct WebsiteHandler {
     public_path: String
 }
 
 impl WebsiteHandler  {
     pub fn new(public_path: String) -> Self {
-
         Self { public_path }
+    }
+
+    fn read_file(&self, file_name: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_name);
+        // ok() convert Result into String or None in not successful
+        fs::read_to_string(path).ok()
     }
 }
 
@@ -18,8 +24,8 @@ impl Handler for WebsiteHandler {
         match request.method() {
 
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok,Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok,Some("<h1>Hello</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok,self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok,self.read_file("hello.html")),
                 _ => Response::new(StatusCode::NotFound, None),
             }
             _ => Response::new(StatusCode::NotFound, None),
