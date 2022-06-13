8c7694441b431dfa502a36f9eb2b107dfa60cee5
ghostfart <appstore@fvnction.net>
2022-06-13 12:07:56 +0200

    add public path

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,23 @@ use http::Request;
 use http::Method;
 use website_handler::WebsiteHandler;
 
+use std::env;
+
 mod http;
 mod server;
 mod website_handler;
 
+
+/**
+ * env macro reads env variables provided by cargo at compile time.
+ * CARGO_MANIFEST_DIR is the directory of Cargo.toml file.
+ * We can see how code looks with vars inserted by running "cargo expand | code".
+ */
 fn main() {
+    let default_path = format!("{}/public",env!("CARGO_MANIFEST_DIR")); 
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    print!("Serving files from: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    // we dont have to pass any params to Handler b/c its an empty struct
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
 
